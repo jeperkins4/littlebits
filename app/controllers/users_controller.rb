e7de15f6b1153e106577f1dd5afa46adb5b11266
John Perkins <jeperkins4@gmail.com>
2017-01-21 20:04:47 -0500

    Refactored user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,38 +16,20 @@ class UsersController < ApplicationController
   end
 
   def create
-    respond_to do |format|
-      if user.save
-        format.html { redirect_to user, notice: 'User was successfully created.' }
-        format.json { render json: user, status: ':created', location: user }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: user.errors, status: ':unprocessable_entity' }
-      end
-    end
+    user.save
+    respond_with user
   end
 
   def update
-    respond_to do |format|
-      if user.update(user_params)
-        format.html { redirect_to user, notice: 'User was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: user.errors, status: ':unprocessable_entity' }
-      end
-    end
+    user.update(user_params)
+    respond_with user
   end
 
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
     user.destroy
-
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
-    end
+    respond_with users, location: users_path
   end
 
   private
--- app/datatables/users_datatable.rb
+++ app/datatables/users_datatable.rb
@@ -2,38 +2,28 @@ class UsersDatatable < BaseDatatable
   delegate :current_user, to: :@view
 
   def as_json(options = {})
-    {
-      sEcho: params[:sEcho].to_i,
-      iTotalRecords: User.count,
-      iTotalDisplayRecords: users.total_count,
-      aaData: data
-    }
+    json_builder(User.count, users.total_count, data)
   end
 
 private
   def data
     users.map do |user|
       [
-        link_to(user.name, user)
+        link_to(user.name, user),
+        links(user)
       ]
     end
   end
 
   def users
-    @users || fetch_users
+    @users || fetch_results(User)
   end
 
-  def fetch_users
-    search_columns = ['users.name']
-    users = User.unscoped.select(search_columns).order(sort_column sort_direction)
-    users = users.page(page_count).per(per_page)
+  def columns
+    ['users.name', 'users.email', 'users.id']
   end
 
-  def sort_column
-    columns[params[:iSortCol_0].to_i]
-  end
-
-  def sort_direction
-    params[:sSortDir_0] == "desc" ? "desc" : "asc"
+  def exceptions
+    ['users.id']
   end
 end
